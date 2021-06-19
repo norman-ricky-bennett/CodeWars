306246852ec982c3a79eb9634ac36afd5708b5a0
norman-ricky-bennett <norman.bennett525@gmail.com>
2021-06-19 08:38:36 -0400

    6/19/21

---
+++ README.md
@@ -0,0 +1,3 @@
+# Codewars Solutions
+
+These are my solutions for coding challenges on codewars.com.
--- disemvowel.cs
+++ disemvowel.cs
@@ -2,12 +2,17 @@ using System;
 
 public static class Kata
 {
+    // method to remove vowels from a given string 
     public static string Disemvowel(string str)
     {
-        str = "This website is for losers LOL!"
-        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
-        string newString = str.TrimEnd(vowels);
-        return newString;
+        string[] vowels = {"a","e","i","o","u","A","E","I","O","U"}; 
+        // iterate through the whole string using a for loop
+        for (int i = 0; i < vowels.Length; i++)
+        {
+            // replace vowels with nothing
+            str = str.Replace(vowels[i],"");  
+        }
+        return str;
     }
    
 }
